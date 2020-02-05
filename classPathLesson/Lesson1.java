4425d5f312fac21741f60dc4dc04cd2bf9784f14
mjpurin <34804764+joytasnet@users.noreply.github.com>
2020-02-05 14:15:28 +0900

    classPathLesson追加

---
+++ classPathLesson/Lesson1.java
@@ -0,0 +1,9 @@
+public class Lesson1{
+	public static void main(String[] args){
+		int[] arr={1,2,3,4,5};
+		util.Common.arrReverse(arr);
+		System.out.println(java.util.Arrays.toString(arr));
+		util.Common.arrShuffle(arr);
+		System.out.println(java.util.Arrays.toString(arr));
+	}
+}
